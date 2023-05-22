c64bd010b29cb75c902fb6077c8c26f63e5226a3
kenser <koffy140221@gmail.com>
2023-05-22 08:09:48 -0600

    Creacion de la clase Producto

---
+++ modelos/insumos.php
@@ -0,0 +1,15 @@
+<?php
+require 'Conexion.php';
+
+class Producto extends Conexion{
+    public $id_insumo;
+    public $nombre;
+    public $marca_producto;
+    public $cantidad_existencia;
+    public $marca;
+    public $nombre_proveedor;
+    public $precio;
+
+
+
+}

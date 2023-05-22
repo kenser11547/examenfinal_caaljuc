07a49e9f7b3b8f2e1e2e254d96fd5cbb835c6a87
kenser <koffy140221@gmail.com>
2023-05-22 08:35:52 -0600

    crear la conexion con la BD e imprimir si no se conecta

--- modelos/Conexion.php
+++ modelos/Conexion.php
@@ -4,6 +4,17 @@ abstract class Conexion{
     public static $conexion = null;
 
     private static function conectar(){
-        
+       try {
+        /*CONEXION CON LA BASE DE DATOS*/
+        self::$conexion =new PDO('informix:host=host.docker.internal; service=9088; database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix');
+       /*DEFINIR LAS EXCEPCIONES*/
+       self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e){
+            /*imprimir el la pantalla el error*/
+            echo "Error de conexion a la Base de Datos";
+            echo "<br>";
+            echo $e->getMessage();
+            exit;
+        }
     }
 }

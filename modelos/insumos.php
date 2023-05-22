a519fbdb046429b41d01231bfa36c23dec2625c6
kenser <koffy140221@gmail.com>
2023-05-22 08:13:08 -0600

    se creo el metodo __construct

--- modelos/insumos.php
+++ modelos/insumos.php
@@ -9,7 +9,17 @@ class Producto extends Conexion{
     public $marca;
     public $nombre_proveedor;
     public $precio;
+ 
+    public function __construct($args = [] )
+    {
+        $this->id_insumo = $args['id_insumo'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->marca_producto = $args['marca_producto'] ?? '';
+        $this->cantidad_existencia = $args['cantidad_existencia'] ?? '';
+        $this->marca = $args['marca'] ?? '';
+        $this->nombre_proveedor = $args['nombre_proveedor'] ?? '';
+        $this->precio = $args['precio'] ?? '';
 
-
+            }
 
 }

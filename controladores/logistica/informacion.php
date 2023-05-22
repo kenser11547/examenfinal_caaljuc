fda36a4cef535808ed714eb9b4e52eb8343dc02c
kenser <koffy140221@gmail.com>
2023-05-22 09:02:14 -0600

    agregar estilo al formulario

--- controladores/logistica/informacion.php
+++ controladores/logistica/informacion.php
@@ -32,7 +32,7 @@ try {
         /*aqui se diseña el cuerpo de la pagina*/
 
         body {
-            background-image: url(./ejercito.jpg);
+            background-image: url(./insumos.jpg);
             background-size:cover;
             background-attachment: fixed;
 
